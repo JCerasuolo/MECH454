6b37144d102191006ca7ce541201cfe74a7a248d
Jordan Cerasuolo <Jordan Cerasuolo>
2017-02-02 18:55:00 -0500

    Same as previous commit (forgot to push some files)

--- Desktop/MECH454Project/MECH454Project/Cylinder.cpp
+++ Desktop/MECH454Project/MECH454Project/Cylinder.cpp
@@ -1,1 +1,24 @@
-Fart Malarky
+#include "stdafx.h"
+#include <iostream>
+
+class Cyli
+{
+private:
+	int compRatio; //Compression ratio
+	double Vd; //Displacement volume (L)
+	double Ve; //Compressed Volume (L)
+	double Vc; //Expanded Volume(L)
+	double index; //Adiabatic Index
+
+public:
+	Cyli()
+	{
+		compRatio = 8;
+		Vd = 1;
+		Vc = 0.143;
+		Ve = 1.143;
+		index = 1.35;
+	}
+
+	~Cyli(){}
+};
--- Desktop/MECH454Project/MECH454Project/ProjectMain.cpp
+++ Desktop/MECH454Project/MECH454Project/ProjectMain.cpp
@@ -2,10 +2,45 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include "IsentropicProcess.cpp"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+class State
 {
+private:
+	double Pr; //Pressure (Pa)
+	double Tmp; //Temperature (K)
+
+public:
+	State(double pretzle, double tetzle)
+	{
+		Pr = pretzle;
+		Tmp = tetzle;
+	}
+};
+
+int main()
+{
+
+	Cyli c1;
+
+	State s1(300,100000);
+	State s2(isenCompressionT(), isenCompressionP());
+	State s3(thermalAddT(), thermalAddP());
+	State s4(isenExpansionT(), isenExpansionP());
+	State returnus(thermalSubT(), thermalSubP());
+
+	qin = 2000; //(kJ/kg)
+
+	cout << s1.Tmp << ", " << s1.Pr << endl;
+	cout << s2.Tmp << ", " << s2.Pr << endl;
+	cout << s3.Tmp << ", " << s3.Pr << endl;
+	cout << s4.Tmp << ", " << s4.Pr << endl;
+	cout << returnus.Tmp << ", " << returnus.Pr << endl;
+
 	return 0;
 }
 
