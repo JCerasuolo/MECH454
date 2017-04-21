802ed3ac2a563ee25c8701cf93af1ea8ea011acc
Yankel Lahiany <yankel.lahiany@gmail.com>
2017-04-21 13:39:45 -0400

    Heat transfer header file

---
+++ HeatTransfer.h
@@ -0,0 +1,8 @@
+#ifndef HEATTRANSFER_H
+#define HEATTRANSFER_H
+
+double sin_sq(double ang);
+double convective_ht();
+double xb(double theta, double theta_s, double theta_d, double a, double n);
+
+#endif
